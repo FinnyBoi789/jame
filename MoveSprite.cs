1a8ccc160454a4b2605801269af6354364cdb069
FinnyBoi789 <146110224+FinnyBoi789@users.noreply.github.com>
2024-10-10 11:52:39 +0100

    Update MoveSprite.cs

--- MoveSprite.cs
+++ MoveSprite.cs
@@ -10,6 +10,10 @@ public class MoveSprite : MonoBehaviour
     public string left;
     public string right;
 
+    public int numJumps = 1;
+
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +25,13 @@ public class MoveSprite : MonoBehaviour
     {
         Vector3 move = new Vector3(0, 0, 0);
 
-        if (Input.GetKey(up))
-        {
-            move = new Vector3(0, 0.03f, 0);
+        if (Input.GetKeyDown(up) && numJumps > 0) 
+        {   
+            rb.velocity = new Vector3(rb.velocity.x, 10, 0);
+            numJumps -= 1;
         }
+
+
 
         if (Input.GetKey(down))
         {
@@ -43,5 +50,21 @@ public class MoveSprite : MonoBehaviour
 
         this.transform.Translate(move);
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            numJumps = 1;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+        }
     }
 }
